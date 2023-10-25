521aa0f301af1ce444e2209c97cf5beea696a562
HAL22 <thethelafaltein@gmail.com>
2023-10-25 19:34:48 +0200

    Changed the frontend

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ st.title("Query any website")
 txt_input = ""
 with st.form(key='txt_input'):
     txt_input = st.text_area('Enter url', '', height=80)
-    agent = cb.get_agent(txt_input)
-    st.form_submit_button(label='Enter') 
+    submit_button = st.form_submit_button(label='Enter')
+    if submit_button:
+          agent = cb.get_agent(txt_input)
 
 
 if "messages" not in st.session_state:
