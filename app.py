64f6550ae66e4bf883af598418cfddbf1a4bfca5
HAL22 <thethelafaltein@gmail.com>
2023-10-30 21:51:58 +0200

    cached resources

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import chatbot as cb
 st.title("Query any website")
 
 txt_input = ""
-agent = None
+agent = any
 with st.form(key='txt_input'):
     txt_input = st.text_area('Enter url', '', height=80)
     submit_button = st.form_submit_button(label='Enter')
     if submit_button:
           agent = cb.get_agent(txt_input)
-
+          st.session_state['agent'] = agent
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -25,5 +25,6 @@ if prompt := st.chat_input("What is up?"):
 
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
+        agent = st.write(st.session_state.agent)
         full_response = agent(prompt)['output']
         message_placeholder.markdown(full_response)
